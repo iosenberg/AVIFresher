e684a58671325ac078afeac0a6621ec4603d8c35
iosenberg <iosenber@oberlin.edu>
2022-01-31 18:55:32 -0500

    Success! Can now link to filtered pages

--- index.php
+++ index.php
@@ -68,28 +68,30 @@
 	<h1>AVI Fresh SUCKS</h1>
 	<!-- Button Test -->
 	<div>
-		<script type="text/javascript">
+		<script language="javascript" type="text/javascript">
 			function makeURL() {
-				var queries = 
-					(document.getElementById('Soy').value ? "Soy=TRUE&") +
-					(document.getElementById('Dairy').value ? "Dairy=TRUE&") +
-					(document.getElementById('Wheat').value ? "Wheat=TRUE&") +
-					(document.getElementById('Vegetarian').value ? "Vegetarian=TRUE&") +
-					(document.getElementById('Vegan').value ? "Vegan=TRUE&") +
-					(document.getElementById('Egg').value ? "Egg=TRUE&") +
-					(document.getElementById('Fish').value ? "Fish=TRUE&") +
-					(document.getElementById('Peanut').value ? "Peanut=TRUE&") +
-					(document.getElementById('Shellfish').value ? "Shellfish=TRUE&") +
-					(document.getElementById('TreeNut').value ? "TreeNut=TRUE&") +
-					(document.getElementById('GlutenSensitive').value ? "GlutenSensitive=TRUE&") +
-					(document.getElementById('Halal').value ? "Halal=TRUE&") +
-					(document.getElementById('Kosher').value ? "Kosher=TRUE&");
+				var queries = "?" +
+					(document.getElementById('Soy').value ? "Soy=TRUE&" : "") +
+					(document.getElementById('Dairy').value ? "Dairy=TRUE&" : "") +
+					(document.getElementById('Wheat').value ? "Wheat=TRUE&" : "") +
+					(document.getElementById('Vegetarian').value ? "Vegetarian=TRUE&" : "") +
+					(document.getElementById('Vegan').value ? "Vegan=TRUE&" : "") +
+					(document.getElementById('Egg').value ? "Egg=TRUE&" : "") +
+					(document.getElementById('Fish').value ? "Fish=TRUE&" : "") +
+					(document.getElementById('Peanut').value ? "Peanut=TRUE&" : "") +
+					(document.getElementById('Shellfish').value ? "Shellfish=TRUE&" : "") +
+					(document.getElementById('TreeNut').value ? "TreeNut=TRUE&" : "") +
+					(document.getElementById('GlutenSensitive').value ? "GlutenSensitive=TRUE&" : "") +
+					(document.getElementById('Halal').value ? "Halal=TRUE&" : "") +
+					(document.getElementById('Kosher').value ? "Kosher=TRUE&" : "");
 
-				window.location = "localhost/AVIFresher/index.php" + queries;
+				location.href = "localhost/AVIFresher/index.php" + queries;
+
+				return true;
 			}
 		</script>
 
-		<form>
+		<form onSubmit="return makeURL();">
 
 			<label for="Soy">Soy:</label>
 			<input type="checkbox" id="Soy" name="Soy">
@@ -130,7 +132,7 @@
 			<label for="Kosher">Kosher:</label>
 			<input type="checkbox" id="Kosher" name="Kosher">
 
-			<input type="button" value="Go" onClick="goToPage();">
+			<input type="submit" value="Go">
 		</form>
 	</div>
 	<div>
